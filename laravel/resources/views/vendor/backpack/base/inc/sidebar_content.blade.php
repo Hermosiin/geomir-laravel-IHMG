70ee532b852ade59602bb0798a3851e985643984
Ismael Hermoso Rivas <ismael.hermoso.rivas@hotmail.com>
2022-11-14 18:51:14 +0100

    modificacion vistas side bar, adaptar controladores

--- laravel/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ laravel/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,28 +1,27 @@
+{{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
+
 @hasanyrole('admin|editor')
    {{ __("Admins and editors section") }}
+   <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+   @hasanyrole('admin')
+   <li class="nav-item nav-dropdown">
+      <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Authentication</a>
+      <ul class="nav-dropdown-items">
+         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> <span>Users</span></a></li>
+         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
+         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
+      </ul>
+   </li>
+   @endhasanyrole
+   <!-- el can sirve para ser mas meticuloso en los permisos -->
+   <!-- @can('admin users')
+      {{ __("You can administer users!") }}
+   @endcan -->
+   <li class="nav-item"><a class="nav-link" href="{{ backpack_url('visibility') }}"><i class="nav-icon la la-question"></i> Visibilities</a></li>
+   <li class="nav-item"><a class="nav-link" href="{{ backpack_url('place') }}"><i class="nav-icon la la-question"></i> Places</a></li>
+   <li class="nav-item"><a class="nav-link" href="{{ backpack_url('post') }}"><i class="nav-icon la la-question"></i> Posts</a></li>
 @else
    {{ __("Only admins and editors can see this section") }}
 @endhasanyrole
  
-@can('admin users')
-   {{ __("You can administer users!") }}
-@endcan
 
-
-{{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('visibility') }}"><i class="nav-icon la la-question"></i> Visibilities</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('place') }}"><i class="nav-icon la la-question"></i> Places</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('post') }}"><i class="nav-icon la la-question"></i> Posts</a></li>
-
-<li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Authentication</a>
-    <ul class="nav-dropdown-items">
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> <span>Users</span></a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
-    </ul>
-</li>
-
-<!-- Mirar el 2.2 si hay que cambiar algo, en la pagina lo hacia con menu desplegable https://github.com/Laravel-Backpack/PermissionManager#install -->
