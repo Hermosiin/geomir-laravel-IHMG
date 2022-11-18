a7fc0f21adf3cb16919a782b4490ffd0f2f87d06
Ismael Hermoso Rivas <ismael.hermoso.rivas@hotmail.com>
2022-11-18 19:06:44 +0100

    extensio backpack editor

--- laravel/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ laravel/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -3,16 +3,30 @@
 @hasanyrole('admin|editor')
    {{ __("Admins and editors section") }}
    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-   @hasanyrole('admin')
-   <li class="nav-item nav-dropdown">
-      <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Authentication</a>
-      <ul class="nav-dropdown-items">
-         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> <span>Users</span></a></li>
-         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
-         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
-      </ul>
-   </li>
-   @endhasanyrole
+   @hasrole('admin')
+      <li class="nav-item nav-dropdown">
+         <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Authentication</a>
+         <ul class="nav-dropdown-items">
+            <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> <span>Users</span></a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
+         </ul>
+      </li>
+   @else
+      {{ __("Only admins can see this section") }}
+   @endhasrole
+   @hasrole('editor')
+      <li class="nav-item nav-dropdown">
+         <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-globe"></i> Translations</a>
+         <ul class="nav-dropdown-items">
+            <li class="nav-item"><a class="nav-link" href="{{ backpack_url('language') }}"><i class="nav-icon la la-flag-checkered"></i> Languages</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ backpack_url('language/texts') }}"><i class="nav-icon la la-language"></i> Site texts</a></li>
+         </ul>
+      </li>
+   @else
+      {{ __("Only editors can see this section") }}
+   @endhasrole
+
    <!-- el can sirve para ser mas meticuloso en los permisos -->
    <!-- @can('admin users')
       {{ __("You can administer users!") }}
