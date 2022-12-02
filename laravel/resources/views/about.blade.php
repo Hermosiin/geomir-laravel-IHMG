8a21a14a9f62322703071cd9be6b6ed48fea7047
Marc Gimenez <marcgimenezalonso27@gmail.com>
2022-12-02 19:59:33 +0100

    M9 a falta de cambiar transicion y poner musica

--- laravel/resources/views/about.blade.php
+++ laravel/resources/views/about.blade.php
@@ -2,7 +2,48 @@
  
 @section('content')
 
-    
+    <div class="div-about">
+        <div class="container">
+            <div class="card">
+                <div class="contenedor-imagenes">
+                    <div class="caja1-marc"></div>
+                    <div class="caja2-marc"></div>
+                </div>
+                <div class="contenido">
+                    <h3>Marc Giménez</h3>
+                    <!-- <p class="trabajo-marc">El Mejor del Mundo</p> -->
+                    <button class="trabajo-marc"></button>
+                    <a href="#">Vídeo</a>
+
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="contenedor-imagenes">
+                    <div class="caja1-marc"></div>
+                    <div class="caja2-marc"></div>
+                </div>
+                <div class="contenido-marc">
+                    <h3>Marc Giménez</h3>
+                    <p>El Mejor del Mundo</p>
+                    <a href="#">Vídeo</a>
+
+                </div>
+
+
+            </div>
+
+
+
+        </div>
+
+
+
+
+
+
+
+    </div>
 
 
 
