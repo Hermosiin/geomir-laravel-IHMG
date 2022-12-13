ddb525f4b71074da3b6e60c225f28f557c9351c0
Marc Gimenez <marcgimenezalonso27@gmail.com>
2022-12-13 16:35:13 +0100

    Canciones puestas a falta de hacer video y pop up

--- laravel/resources/views/about.blade.php
+++ laravel/resources/views/about.blade.php
@@ -8,7 +8,9 @@
                 <div class="contenedor-imagenes">
                     <div class="caja1-marc" id="fotoAudio1"></div>
                     <div class="caja2-marc"></div>
-                    <audio src="./img/jul.mp3" class="enter1"></audio>
+                    <audio id="jul">
+                        <source src="./img/jul5.mp3" type="audio/mp3"></source>
+                    </audio>
                 </div>
                 <div class="contenido">
                     <h3>Marc Giménez</h3>
@@ -22,7 +24,9 @@
                 <div class="contenedor-imagenes">
                     <div class="caja1-ismael" id="fotoAudio2"></div>
                     <div class="caja2-ismael"></div>
-                    <audio src="./img/shakira.mp3" class="enter2"></audio>
+                    <audio id="shakira">
+                        <source src="./img/shakira.mp3" type="audio/mp3"></source>
+                    </audio>
 
                 </div>
                 <div class="contenido">
@@ -46,19 +50,52 @@
     
     <script>
 
-        let more1 = document.getElementById()('enter1');
-        let more2 = document.getElementById()('enter2');
+        let jul = document.getElementById('jul');
+        let shakira = document.getElementById('shakira');
 
-        let audio = document.getElementsByTagName('audio');
 
-        more1.addEventListener('mouseenter', () => {
+        var imagen1=document.getElementById('fotoAudio1')
 
-            audio[0].play()
-        });
-        more2.addEventListener('mouseenter', () => {
+        window.addEventListener('load', iniciar1, false);
+        window.addEventListener('load', parar1, false);
 
-            audio[1].play()
-        });
+        function iniciar1(){
+            imagen1.addEventListener('mouseover', iniciar1F, false);
+        }
+
+        function parar1(){
+            imagen1.addEventListener('mouseout', parar1F, false);
+        }
+
+        function parar1F(){
+            jul.pause();
+        }
+
+        function iniciar1F(){
+            jul.play();
+        }
+
+        var imagen2=document.getElementById('fotoAudio2')
+
+        window.addEventListener('load', iniciar2, false);
+        window.addEventListener('load', parar2, false);
+
+        function iniciar2(){
+            imagen2.addEventListener('mouseover', iniciar2F, false);
+        }
+
+        function parar2(){
+            imagen2.addEventListener('mouseout', parar2F, false);
+        }
+
+        function parar2F(){
+            shakira.pause();
+        }
+
+        function iniciar2F(){
+            shakira.play();
+        }
+        
         
 
     </script>
