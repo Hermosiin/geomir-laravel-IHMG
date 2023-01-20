9ee43374efe41cbb94397db7fccaab0a54e59fa9
Marc Gimenez <marcgimenezalonso27@gmail.com>
2023-01-20 16:03:58 +0100

    Nuevos cambios m09

--- laravel/resources/views/contact.blade.php
+++ laravel/resources/views/contact.blade.php
@@ -6,11 +6,14 @@
     ::-webkit-scrollbar {
     display: none;
 }
+
 </style>
 <div class="flex-contact">
     <div class="fondo-cont">
         <video src="./img/quemiras.mp4" class="video-que-miras" autoplay="true" muted="true" loop="true"></video>
-
+        <div class="texto-encima">CONTACTA'NS!!!</div>
+        <div class="texto-encima2">Envia el teu missatge</div>
+        <button class="boton-contacto">Formulari de contacte</button>
     </div>
         
     <div class="mapa-contact">
