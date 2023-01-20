7cb52e69bbc07f385060c3a9847155160e9abff9
Ismael Hermoso Rivas <ismael.hermoso.rivas@hotmail.com>
2023-01-20 17:01:50 +0100

    Part footer m09 feta

--- laravel/resources/views/contact.blade.php
+++ laravel/resources/views/contact.blade.php
@@ -19,5 +19,32 @@
     <div class="mapa-contact">
 
     </div>
+
+    <footer class="pie-pagina">
+        <div class="grupo-1">
+            <div class="box">
+                <figure>
+                    <a href="#">
+                        <img src="public" alt="Logo de Geomir">
+                    </a>
+                </figure>
+            </div>
+            <div class="box">
+                <h2>SOBRE NOSOTROS</h2>
+                <p>Pagina dedica al videjuego Counter Strike.</p>
+                <p>Tenemos todo tipo de informacion sobre armas, mapas, modos y mejores jugadores del momento.</p>
+            </div>
+            <div class="box">
+                <h2>SIGUENOS</h2>
+                <div class="red-social">
+                    <a href="https://es-la.facebook.com/CSGLOB/" target="_blank" class="fa fa-facebook"></a>
+                    <a href="https://www.instagram.com/hermosiin/?hl=es" target="_blank" class="fa fa-instagram"></a>
+                    <a href="https://twitter.com/hermosiin" target="_blank" class="fa fa-twitter"></a>
+                    <a href="https://www.youtube.com/c/FlipiNLosFieles" target="_blank" class="fa fa-youtube"></a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </div>
+
 @endsection
